86c408c6ddf15f3f96383994610986e429582626
progali007 <code.progali@gmail.com>
2023-05-01 11:13:12 +0430

    Create a function to read data from the local storage

--- src/features/handel-tasks/taskSlice.ts
+++ src/features/handel-tasks/taskSlice.ts
@@ -1,11 +1,18 @@
-import { readTasksFromStorage } from "../../utils/storage";
-
 type task = {
   id: string;
   title: string;
   category: "personal" | "work" | "other";
   state: "complete" | "incomplete";
   date: string;
+};
+
+const readTasksFromStorage = () => {
+  const taskListData = localStorage.getItem("tasks");
+  if (taskListData) return JSON.parse(taskListData);
+
+  // If there is no item
+  localStorage.setItem("tasks", JSON.stringify([]));
+  return [];
 };
 
 export const useTask = (set) => ({
