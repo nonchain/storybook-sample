ba6e3ca7a1e7304fa404e45a03d38df3146f1b69
progali007 <code.progali@gmail.com>
2023-05-01 12:50:27 +0430

    Implement modal slice

--- src/app/stor.ts
+++ src/app/stor.ts
@@ -1,6 +1,8 @@
 import { create } from "zustand";
 import { useTask } from "../features/handel-tasks/taskSlice";
+import { useModal } from "../features/modal/modalSlice";
 
 export const useStore = create((...set) => ({
   ...useTask(...set),
+  ...useModal(...set)
 }));
